65c1a0d06578294e15869cba196a2e6f36cb3dd6
Jimmy <jamestaylorson@gmail.com>
2020-07-17 10:21:37 -0400

    Hide and show

--- staging/baby-names/js/main.js
+++ staging/baby-names/js/main.js
@@ -1,24 +1,34 @@
+// Sets the viewport to 100% minus the address bar
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
-$(document).ready(function(){
-  $(".btn1").click(function(){
+// Fade out cover screen on tap/click
+$(document).ready(function () {
+  $(".btn1").click(function () {
     $(".fadeDiv").fadeOut(500);
   });
 });
 
-$(document).on("keypress", "input", function(e){
-    if(e.which == 13){
-        $(this).focusout().blur();
-    }
+// Accept enter/rtn key on input field
+$(document).on("keypress", "input", function (e) {
+  if (e.which == 13) {
+    $(this).focusout().blur();
+  }
 });
 
-$(function() {
-$(".form__field").focusin(function() {
+// Hide and show spin button when input field is in and out of focus
+$(function () {
+  $(".form__field").focusin(function () {
     $(".spinButton").hide();
-}).focusout(function () {
+  }).focusout(function () {
     $(".spinButton").show(100);
+  });
 });
+
+// Hide coffee div 5s
+$(document).ready(function () {
+  $("#hideMe").hide();
+  $("#hideMe").delay(5000).fadeIn(500);
 });
