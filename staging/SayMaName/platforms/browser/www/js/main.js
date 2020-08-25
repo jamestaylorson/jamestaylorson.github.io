ffb29435f4a655d1c3ffdfbee3234ce5e13a685a
Jimmy <jamestaylorson@gmail.com>
2020-08-25 11:40:44 -0400

    staging removed

--- staging/SayMaName/platforms/browser/www/js/main.js
+++
@@ -1,7 +0,0 @@
-// Sets the viewport to 100% minus the address bar
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
